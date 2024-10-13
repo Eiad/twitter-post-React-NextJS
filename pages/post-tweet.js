f3162aa7d62ce4fb10232725569a21c12ca721d6
Ash <4707734+Eiad@users.noreply.github.com>
2024-10-13 11:07:09 +0400

    Updates

--- pages/post-tweet.js
+++ pages/post-tweet.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { Container } from '@mui/material';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import PostTweetComponent from '../components/PostTweetComponent';
+import PostTweetComponent from '../components/PostTweetComponent/PostTweetComponent';
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#FF9A8B', // Light pink
+      main: '#FF9A8B', 
     },
     secondary: {
-      main: '#FF6A88', // Darker pink
+      main: '#FF6A88', 
     },
     background: {
-      default: '#FFF6F6', // Very light pink background
+      default: '#FFF6F6', 
     },
     text: {
-      primary: '#4A4A4A', // Dark gray for text
+      primary: '#4A4A4A',
     },
   },
 });
